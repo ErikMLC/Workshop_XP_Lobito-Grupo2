30d34b37d0500a6d67f853ae6785e96844356e2d
AId3n <106205670+KingP1g@users.noreply.github.com>
2023-12-25 18:02:21 -0500

    refactor mamadisimo

--- 3.Coding/src/Implementacion.java
+++ 3.Coding/src/Implementacion.java
@@ -1,61 +1,48 @@
-
+import java.util.Arrays;
 public class Implementacion {
-    public String[] personajes = {"C", "L", "U"};
-    public String[] salida = {"C", "L", "U"};
-    public String[] llegada = new String[3];
+    public String[] personajes = {"C", "L", "U"}, salida = {"C", "L", "U"}, llegada = new String[3];
     public int posicion = 0; // Departure 0, llegada 1
-
     public void imprimirEstado() {
-        System.out.println("\t ##### Problema del Lobo, la Caperucita Roja y las Uvas ##### ");
-        System.out.println("\nPERSONAJES: ");
-        System.out.println("\n< L > Lobo, < C > Caperucita, < U > Uvas, < N > Nada (No lleva Nada)");
-        System.out.print("\nPersonajes en Salida: ");
+        System.out.println("\t ##### Problema del Lobo, la Caperucita Roja y las Uvas ##### \n");
+        System.out.println("\nPERSONAJES: \n");
+        System.out.println("\nL:Lobo, C:Caperucita, U:Uvas, N:Nada (No lleva Nada)\n");
+        System.out.print("Personajes en Salida: ");
         imprimirPersonajes(salida);
-        System.out.print("\nPersonajes en llegada: ");
+        System.out.print("Personajes en llegada: ");
         imprimirPersonajes(llegada);
         String estado = (posicion == 0) ? "Salida" : "Llegada";
         System.out.printf("\nOrilla actual: %s%n", estado);
     }
-
     private void imprimirPersonajes(String[] personajes) {
-        for (String personaje : personajes) {
-            if (personaje != null) {
-                System.out.print(personaje + " ");
-            }
-        }
+        Arrays.stream(personajes)
+                .filter(personaje -> personaje != null)
+                .forEach(personaje -> System.out.print(personaje + " "));
         System.out.println();
     }
-
     public void movimientos(String movimientoPersonaje) {
         if (movimientoPersonaje.equals("N")) {
             posicion = 1 - posicion;
             return;
         }
-        int index = encontrarPersonaje(movimientoPersonaje, posicion == 0 ? salida : llegada);
-        if (index == -1) {
+
+        String[] origen = (posicion == 0) ? salida : llegada;
+        String[] destino = (posicion == 0) ? llegada : salida;
+
+        int index = encontrarPersonaje(movimientoPersonaje, origen);
+        if (index == -1 || origen[index] == null) {
             System.out.println("No se encuentra la abreviatura de ese Personaje");
             return;
         }
-        if (posicion == 0) {
-            salida[index] = null;
-            for (int i = 0; i < llegada.length; i++) {
-                if (llegada[i] == null) {
-                    llegada[i] = movimientoPersonaje;
-                    break;
-                }
-            }
-        } else {
-            llegada[index] = null;
-            for (int i = 0; i < salida.length; i++) {
-                if (salida[i] == null) {
-                    salida[i] = movimientoPersonaje;
-                    break;
-                }
+        origen[index] = null;
+        for (int i = 0; i < destino.length; i++) {
+            if (destino[i] == null) {
+                destino[i] = movimientoPersonaje;
+                break;
             }
         }
+
         posicion = 1 - posicion;
     }
-
     private int encontrarPersonaje(String personaje, String[] personajes) {
         for (int i = 0; i < personajes.length; i++) {
             if (personaje.equals(personajes[i])) {
@@ -64,39 +51,22 @@ public class Implementacion {
         }
         return -1;
     }
-
     public boolean ganador() {
-        for (String personaje : salida) {
-            if (personaje != null) {
-                return false;
-            }
-        }
-        return true;
+        return Arrays.stream(salida).allMatch(personaje -> personaje == null);
     }
 
     public boolean perdidaSalida() {
         if (posicion == 0) {
-            for (String personaje : salida) {
-                if (personaje != null) {
-                    return false;
-                }
-            }
-            return true;
+            return Arrays.stream(salida).allMatch(personaje -> personaje == null);
         } else {
-            if ((encontrarPersonaje("C", salida) != -1 && encontrarPersonaje("L", salida) != -1)
-                    || (encontrarPersonaje("C", salida) != -1 && encontrarPersonaje("U", salida) != -1)) {
-                return true;
-            }
+            return (encontrarPersonaje("C", salida) != -1 && encontrarPersonaje("L", salida) != -1)
+                    || (encontrarPersonaje("C", salida) != -1 && encontrarPersonaje("U", salida) != -1);
         }
-        return false;
     }
-
     public boolean perdidaLlegada(String movimientoPersonaje) {
         if (movimientoPersonaje.equals("N")) {
-            if ((encontrarPersonaje("C", llegada) != -1 && encontrarPersonaje("L", llegada) != -1)
-                    || (encontrarPersonaje("C", llegada) != -1 && encontrarPersonaje("U", llegada) != -1)) {
-                return true;
-            }
+            return (encontrarPersonaje("C", llegada) != -1 && encontrarPersonaje("L", llegada) != -1)
+                    || (encontrarPersonaje("C", llegada) != -1 && encontrarPersonaje("U", llegada) != -1);
         }
         return false;
     }
